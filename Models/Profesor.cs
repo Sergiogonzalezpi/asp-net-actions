8b4c197d05fd2f3c684385de9856355f364cda15
Sergiogonzalezpi <sergio.gonzalezpi@alumnos.upm.es>
2021-05-25 12:34:57 +0200

    First commit

---
+++ Models/Profesor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_ACTIONS.Models
+{
+    public class Profesor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class ProfDBContext : DbContext
+    {
+        public DbSet<Profesor> Profesores { get; set; }
+    }
+}
